022de49c9d949c4270c0d303a3fc43dd7ea7078d
twilight-wizard <john.grillo@pcc.edu>
2016-12-04 12:51:47 -0800

    trying to render chart.js

--- lab/app.js
+++ lab/app.js
@@ -131,6 +131,7 @@ function clickHandler(event) {
     endTest.innerHTML = '<h1> THIS IS THE END OF TEST. HERE ARE THE CHART RESULTS. </h1>';
     alert(msg);
     console.log(msg);
+    chartConfig.render();
   }
   //update global counter to quit test.
   totalClicks += 1;
@@ -182,8 +183,41 @@ function addClicks() {
 **************************************/
 
 //this should create the new chart in canvas
-var ctx = document.getElementById('my_chart');
-var chartConfig = {
-
-};
-var myChart = new Chart(ctx, {});
+// code liberated from chart.js documentation default example
+var ctx = document.getElementById('my_chart').getContext('2d');
+var chartConfig = new Chart (ctx, {
+      type: 'bar',
+      data: {
+          labels: 'Market Test Results',
+          datasets: [{
+              label: items.name,
+              data: items.clicked,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
